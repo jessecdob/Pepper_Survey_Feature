e73df93093b21fb8cacece7c8bd45df206f0b103
Jesse Cheung <jesse.cheung67@gmail.com>
2022-02-05 15:07:58 -0800

    Included more boxes and organized it around for the survey part of the feature

--- translations/translation_en_US.ts
+++ translations/translation_en_US.ts
@@ -15,4 +15,27 @@
             <translation type="unfinished">Hello, would you like to do a survey? </translation>
         </message>
     </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello, would you like to do a survey? </source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello, would you like to do a survey? </translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Sorry, the survey could not load.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Sorry, the survey could not load.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Is there anything else I can help you with?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Is there anything else I can help you with?</translation>
+        </message>
+    </context>
 </TS>
